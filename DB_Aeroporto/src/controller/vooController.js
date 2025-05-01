54b6e0fa9b4c207863acaa192eb8c98e07c7201f
marcosfelipe013 <128655961+marcosfelipe013@users.noreply.github.com>
2025-04-30 21:36:19 -0300

    Add files via upload

---
+++ DB_Aeroporto/src/controller/vooController.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const {request} = require('express');
+const {matchedData} = require('express-validator');
+const Voo = require('../model/Voo');
+
+module.exports = {
+    editVoo: async(request, response) => {
+        const data = matchedData(request);
+        let updates = {};
+
+        if(data.nmrVoo) {
+            updates.nmrVoo = data.nmrVoo;
+        }
+        if(data.origem) {
+            updates.origem = data.origem;
+        }
+        if(data.destino) {
+            updates.destino = data.destino;
+        }
+        if(data.dataHrPartida) {
+            updates.dataHrPartida = data.dataHrPartida;
+        }
+        if(data.portaoId) {
+            updates.portaoId = data.portaoId;
+        }
+        if(data.status) {
+            updates.status = data.status;
+        }
+
+        await Voo.findByIdAndUpdate(data.id, updates, {new: true});
+        response.status(200).json({message: 'Voo atualizado com sucesso!'});
+    },
+    deleteVoo: async(request, response) => {
+        const data = matchedData(request);
+        await Voo.findByIdAndDelete(data.id);
+        response.status(200).json({message: 'Voo deletado com sucesso!'});
+    },
+    getVoo: async(request, response) => {
+        const data = matchedData(request);
+        const voo = await Voo.findById(data.id);
+        if(!voo) {
+            response.status(400).json({message: 'Voo não encontrado!'});
+            return;
+        }
+        response.status(200).json(voo);
+    },
+    getAllVoos: async(request, response) => {
+        const voos = await Voo.find();
+
+        if(!voos) {
+            response.status(400).json({message: 'Nenhum voo encontrado!'});
+            return;
+        }
+        response.status(200).json(voos);
+    },
+    updateStatus: async(request, response) => {
+        const data = matchedData(request);
+        const voo = await Voo.findById(data.id);
+
+        if(!voo) {
+            response.status(400).json({message: 'Voo não encontrado!'});
+            return;
+        }
+
+        voo.status = data.status;
+        await voo.save();
+        response.status(200).json({message: 'Status do voo atualizado com sucesso!'});
+    }
+};
---
+++ DB_Aeroporto/src/model/passageiro.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const modelSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    cpf: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    vooId: {
+        type: String,
+        required: true
+    },
+    statusCheckIn: {
+        type: String,
+        default: "Pendente"
+    }
+});
+
+const modelName = 'Passageiros';
+
+if (mongoose.connection && mongoose.connection.models[modelName]) {
+    module.exports = mongoose.connection.models[modelName];
+}
+else {
+    module.exports = mongoose.model(modelName, modelSchema);
+}
