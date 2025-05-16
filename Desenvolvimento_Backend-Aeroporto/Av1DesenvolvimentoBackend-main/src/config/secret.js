8c0ce6b7398a55cdcf9d1d929403f415390bee07
marcosfelipe013 <128655961+marcosfelipe013@users.noreply.github.com>
2025-05-16 19:29:42 -0300

    Delete Desenvolvimento_Backend-Aeroporto directory

--- Desenvolvimento_Backend-Aeroporto/Av1DesenvolvimentoBackend-main/src/config/secret.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET || 'sua_chave_secreta_fallback',
-};
--- Desenvolvimento_Backend-Aeroporto/Av1DesenvolvimentoBackend-main/src/repository/authRepository.js
+++
@@ -1,7 +0,0 @@
-const Funcionario = require('../model/funcionario');
-
-module.exports = {
-    findByEmail: async (email) => {
-        return await Funcionario.findOne({ email });
-    }
-};
